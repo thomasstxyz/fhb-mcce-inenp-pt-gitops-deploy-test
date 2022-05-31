fbc6b8575d7a4e736f1360a62d673b5e786cde5a
thomasstxyz <thomas@thomasst.xyz>
2022-06-01 00:14:41 +0200

    add 2 Terraform plans for s3 bucket and lambda

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# fhb-mcce-inenp-pt-gitops-deploy
+# Description
+
+This repository contains:
+
+- Lambda functions source in `lambda/functions/`:
+    - deleteTodo
+    - getTodos
+    - updateTodo
+- Terraform plans:
+    - `terraform/lambda/`, which provisions the functions.
+    - `terraform/s3/`, which creates an S3 bucket for static website hosting.
---
+++ terraform/lambda/main.tf
@@ -0,0 +1,141 @@
+# Archive a file to be used with Lambda using consistent file mode
+data "archive_file" "lambda_deleteTodo" {
+  type             = "zip"
+  source_file      = "${path.module}/../../lambda/functions/deleteTodo/index.js"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/files/lambda-deleteTodo.js.zip"
+}
+
+data "archive_file" "lambda_getTodos" {
+  type             = "zip"
+  source_file      = "${path.module}/../../lambda/functions/getTodos/index.js"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/files/lambda-getTodos.js.zip"
+}
+
+data "archive_file" "lambda_updateTodo" {
+  type             = "zip"
+  source_file      = "${path.module}/../../lambda/functions/updateTodo/index.js"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/files/lambda-updateTodo.js.zip"
+}
+
+# iam role needed by lambda
+data "aws_iam_role" "LabRole" {
+  name = "LabRole"
+}
+
+resource "aws_lambda_function" "deleteTodo" {
+  filename      = data.archive_file.lambda_deleteTodo.output_path
+  function_name = "deleteTodo-function"
+  role          = data.aws_iam_role.LabRole.arn
+  handler       = "index.js"
+
+  source_code_hash = filebase64sha256(data.archive_file.lambda_deleteTodo.output_path)
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      app = "todo"
+    }
+  }
+
+  depends_on = [
+    data.archive_file.lambda_deleteTodo
+  ]
+}
+
+resource "aws_lambda_function_url" "deleteTodo" {
+  function_name      = aws_lambda_function.deleteTodo.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["POST"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["access-control-allow-origin"]
+    max_age           = 86400
+  }
+}
+
+resource "aws_lambda_function" "getTodos" {
+  filename      = data.archive_file.lambda_getTodos.output_path
+  function_name = "getTodos-function"
+  role          = data.aws_iam_role.LabRole.arn
+  handler       = "index.js"
+
+  source_code_hash = filebase64sha256(data.archive_file.lambda_getTodos.output_path)
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      app = "todo"
+    }
+  }
+
+  depends_on = [
+    data.archive_file.lambda_getTodos
+  ]
+}
+
+resource "aws_lambda_function_url" "getTodos" {
+  function_name      = aws_lambda_function.getTodos.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["POST"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["access-control-allow-origin"]
+    max_age           = 86400
+  }
+}
+
+resource "aws_lambda_function" "updateTodo" {
+  filename      = data.archive_file.lambda_updateTodo.output_path
+  function_name = "updateTodo-function"
+  role          = data.aws_iam_role.LabRole.arn
+  handler       = "index.js"
+
+  source_code_hash = filebase64sha256(data.archive_file.lambda_updateTodo.output_path)
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      app = "todo"
+    }
+  }
+
+  depends_on = [
+    data.archive_file.lambda_updateTodo
+  ]
+}
+
+resource "aws_lambda_function_url" "updateTodo" {
+  function_name      = aws_lambda_function.updateTodo.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = false
+    allow_origins     = ["*"]
+    allow_methods     = ["POST"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["access-control-allow-origin"]
+    max_age           = 86400
+  }
+}
+
+output "url_deleteTodo" {
+  value = aws_lambda_function_url.deleteTodo.function_url
+}
+output "url_getTodo" {
+  value = aws_lambda_function_url.getTodos.function_url
+}
+output "url_updateTodo" {
+  value = aws_lambda_function_url.updateTodo.function_url
+}
